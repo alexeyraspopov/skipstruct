48424a24f766053b87299e9e29fab1436097b462
Oleksii Raspopov <oleksii.raspopov@gmail.com>
2024-10-14 07:26:39 -0400

    generate dts file on build

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,43 @@
 import { pipeline } from "node:stream/promises";
+import { readFileSync } from "node:fs";
+import { basename, relative } from "node:path";
 import { src, dest } from "gulp";
+import { format } from "prettier";
 import { rollup } from "rollup";
+import ts from "typescript";
+import Vinyl from "vinyl";
 
 export async function build() {
   let bundle = await rollup({ input: "skipstruct.js" });
+  await bundle.write({ dir: "build", format: "esm" });
   await Promise.all([
-    bundle.write({ dir: "build", format: "esm" }),
+    pipeline(src(["build/*.js"]), declarations, dest("build")),
     pipeline(src("LICENSE"), dest("build")),
     pipeline(src("package.json"), map(generatePackageJSON), dest("build")),
   ]);
+}
+
+async function* declarations(source) {
+  let config = { allowJs: true, declaration: true, emitDeclarationOnly: true, skipLibCheck: true };
+  let host = ts.createCompilerHost(config);
+  let roots = new Map();
+  let output = new Set();
+  host.writeFile = (fileName, contents) => {
+    output.add(new Vinyl({ path: basename(fileName), contents: Buffer.from(contents) }));
+  };
+  host.readFile = (fileName) => {
+    return roots.get(fileName)?.contents.toString() ?? readFileSync(fileName, "utf8");
+  };
+  for await (let file of source) {
+    roots.set(relative(process.cwd(), file.path), file);
+  }
+  ts.createProgram(Array.from(roots.keys()), config, host).emit();
+  for (let file of output) {
+    let contents = file.contents.toString();
+    let formatted = await format(contents, { parser: "typescript" });
+    file.contents = Buffer.from(formatted);
+    yield file;
+  }
 }
 
 function generatePackageJSON(contents) {
@@ -25,6 +54,7 @@ function generatePackageJSON(contents) {
     type: "module",
     main: "./skipstruct.js",
     module: "./skipstruct.js",
+    types: "./skipstruct.d.ts",
     exports: {
       ".": "./skipstruct.js",
     },
